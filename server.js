f42bd395948f66d44f46eff149b1200fd9bc2a65
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-05-26 16:32:36 -0700

    changes to routes and adding get requests

--- server.js
+++ server.js
@@ -30,6 +30,86 @@ const db = new sqlite3.Database("./db/election.db", err => {
 //     });
 // });
 
+
+//In the preceding statement, the db object is using the all() method. This method runs the SQL query and executes the callback with all the resulting rows that match the query.
+// db.all(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// })
+
+
+//GET a single candidate
+db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log(row);
+})
+
+
+
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+
+
+
+
+// Delete a candidate
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result, this, this.changes);
+// });
+
+
+//Get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates
+                WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if(err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+
+
+        res.json({
+            message: 'success',
+            data: row
+        })
+    })
+})
+
+
+//CREATE a single candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//             VALUES (?,?,?,?)`;
+// const params = [1, "Ronald", "Firbank", 1];
+// //ES5 function, not arrow function, to use this
+// db.run(sql, params, function(err, result) {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result, this.lastID);
+// })
+
+
 //default response for any other request (Not Found) Catch all
 app.use((req, res) => {
     res.status(404).end();
