2cf7e703966233dc318f0478e1533ed3f33ecb02
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-05-27 14:10:54 -0700

    add post route

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require("express");
 //importing sqlite3
 const sqlite3 = require("sqlite3").verbose();
+//importing input check from Utils
+const inputCheck = require("./utils/inputCheck");
 
 //adding PORT
 const PORT = process.env.PORT || 3001;
@@ -96,17 +98,7 @@ app.get('/api/candidate/:id', (req, res) => {
   });
 
 
-//CREATE a single candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//             VALUES (?,?,?,?)`;
-// const params = [1, "Ronald", "Firbank", 1];
-// //ES5 function, not arrow function, to use this
-// db.run(sql, params, function(err, result) {
-//     if(err) {
-//         console.log(err);
-//     }
-//     console.log(result, this.lastID);
-// })
+
 
 
 
@@ -128,6 +120,43 @@ app.delete('/api/candidate/:id', (req, res) => {
   });
 
 
+
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+    VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use `this`
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
+        });
+    });
+  });
+
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//             VALUES (?,?,?,?)`;
+// const params = [1, "Ronald", "Firbank", 1];
+// //ES5 function, not arrow function, to use this
+// db.run(sql, params, function(err, result) {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result, this.lastID);
+// })
 
 
 //default response for any other request (Not Found) Catch all
