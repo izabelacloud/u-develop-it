21969776f138f5c02799612a727e3ee9b6f5e2eb
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-05-27 16:37:04 -0700

    updates to parties calls, adding put

--- server.js
+++ server.js
@@ -155,6 +155,89 @@ app.post('/api/candidate', ({ body }, res) => {
     });
   });
 
+
+  //route to update the candidate table with PUT request
+  app.put("/api/candidate/:id", (req, res) => {
+        const errors = inputCheck(req.body, 'party_id');
+        if(errors) {
+          res.status(400).json({error: errors});
+          return;
+        }
+
+        
+        const sql = `UPDATE candidates SET party_id = ?
+                    WHERE id = ?`;
+        const params = [req.body.party_id, req.params.id];
+
+        db.run(sql, params, function(err, result) {
+            if(err) {
+                res.status(400).json({error: err.message})
+                return;
+            }
+
+        res.json({
+            message: "success",
+            data: req.body,
+            changes: this.changes
+        })
+    })
+  })
+
+
+
+//get route to all parties table
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+
+  //get route to 1 party table
+  app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+
+//delete route for the parties
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      }
+  
+      res.json({ 
+          message: 'successfully deleted', 
+          changes: this.changes });
+    });
+  });
+
+
 // const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
 //             VALUES (?,?,?,?)`;
 // const params = [1, "Ronald", "Firbank", 1];
